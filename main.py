169c5cf572988bd9fe18613df46224a2a28379f4
Nilson Santos <contato@nilson.com.br>
2020-07-08 12:52:19 -0300

    remoção da classe inclusão da função send_image inclusão da função make_speed

--- main.py
+++ main.py
@@ -13,34 +13,38 @@ BOT = telepot.Bot(secrets['BOT'])
 CHAT = secrets['CHAT']
 
 
-class Grafico():
-    def __init__(self, download, upload, download_expected, upload_expected, ping):
-        self.download = download
-        self.upload = upload
-        self.download_expected = download_expected
-        self.upload_expected = upload_expected
-        self.ping = ping
+def graph(download, upload, download_expected, upload_expected, ping):
+    plt.figure()
+    plt.plot(download, '-o', color='blue', label='Download Speed Atual')
+    plt.plot(upload, '-o', color='brown', label='Upload Speed Atual')
+    plt.plot(download_expected, color='green', label='Download Speed Esperado')
+    plt.plot(upload_expected, color='orange', label='Upload Speed Esperado')
+    plt.plot(ping, color='purple', label='Ping')
+    plt.title('Velocidade de Conexão Atual VS. Velocidade de Conexão Esperada \no teste roda a cada 5 minutos')
+    plt.ylabel('Mbps')
+    plt.xlabel('www.nilson.com.br')
+    plt.legend()
+    plt.yticks(np.arange(0, 160, step=10))
+    plt.grid(True)
+    plt.xticks(color='w')
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    send_image(buf)
+    buf.close()
+    plt.close('all')
 
-    def plotar(self):
-        plt.figure()
-        plt.plot(self.download, '-o', color='blue', label='Download Speed')
-        plt.plot(self.upload, '-o', color='brown', label='Upload Speed')
-        plt.plot(self.download_expected, color='green', label='Download Speed Expected')
-        plt.plot(self.upload_expected, color='orange', label='Upload Speed Expected')
-        plt.plot(self.ping, color='purple', label='Ping')
-        plt.title('Velocidade de Conexão Atual VS. Velocidade de Conexão Esperada', )
-        plt.ylabel('Mbps')
-        plt.xlabel('www.nilson.com.br')
-        plt.legend()
-        plt.yticks(np.arange(0, 160, step=10))
-        plt.grid(True)
-        plt.xticks(color='w')
-        buf = io.BytesIO()
-        plt.savefig(buf, format='png')
-        buf.seek(0)
-        BOT.sendPhoto(CHAT, buf)
-        buf.close()
-        plt.close('all')
+
+def make_speedtest():
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    return s.results.dict()
+
+
+def send_image(buf):
+    BOT.sendPhoto(CHAT, buf)
 
 
 while True:
@@ -51,17 +55,13 @@ while True:
     ping_list = []
 
     for _ in range(48):
-        s = speedtest.Speedtest()
-        s.get_best_server()
-        s.download()
-        s.upload()
-        results_dict = s.results.dict()
-
+        results_dict = make_speedtest()
         down_list.append(round(results_dict["download"] * 9.53674E-7, 2))
         up_list.append(round(results_dict["upload"] * 9.53674E-7, 2))
         down_expected_list.append(100)
         up_expected_list.append(50)
         ping_list.append(int(results_dict["ping"]))
 
-        Grafico(down_list, up_list, down_expected_list, up_expected_list, ping_list).plotar()
-        sleep(60)
+        sleep(300)
+
+    graph(down_list, up_list, down_expected_list, up_expected_list, ping_list)
