247229de87a03a8701115a4a2a070f8bd6ed4de2
Nilson Santos <contato@nilson.com.br>
2020-07-08 11:52:18 -0300

    inclusão do ping inclusão da lib speedtest ajustes de variáveis

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import io
 import json
 import telepot
-import subprocess
+import speedtest
 import numpy as np
 from time import sleep
 import matplotlib.pyplot as plt
@@ -14,11 +14,12 @@ CHAT = secrets['CHAT']
 
 
 class Grafico():
-    def __init__(self, download, upload, download_expected, upload_expected):
+    def __init__(self, download, upload, download_expected, upload_expected, ping):
         self.download = download
         self.upload = upload
         self.download_expected = download_expected
         self.upload_expected = upload_expected
+        self.ping = ping
 
     def plotar(self):
         plt.figure()
@@ -26,11 +27,12 @@ class Grafico():
         plt.plot(self.upload, '-o', color='brown', label='Upload Speed')
         plt.plot(self.download_expected, color='green', label='Download Speed Expected')
         plt.plot(self.upload_expected, color='orange', label='Upload Speed Expected')
+        plt.plot(self.ping, color='purple', label='Ping')
         plt.title('Velocidade de Conexão Atual VS. Velocidade de Conexão Esperada', )
         plt.ylabel('Mbps')
-        plt.xlabel('O teste roda a cada 5 Minutos')
+        plt.xlabel('www.nilson.com.br')
         plt.legend()
-        plt.yticks(np.arange(0, 150, step=10))
+        plt.yticks(np.arange(0, 160, step=10))
         plt.grid(True)
         plt.xticks(color='w')
         buf = io.BytesIO()
@@ -46,15 +48,19 @@ while True:
     up_list = []
     down_expected_list = []
     up_expected_list = []
+    ping_list = []
 
     for _ in range(48):
-        j = json.loads(subprocess.check_output(['speedtest-cli', '--json']))
+        s = speedtest.Speedtest()
+        s.download()
+        s.upload()
+        results_dict = s.results.dict()
 
-        down_list.append(round(j["download"] * 9.53674E-7, 2))
-        up_list.append(round(j["upload"] * 9.53674E-7, 2))
+        down_list.append(round(results_dict["download"] * 9.53674E-7, 2))
+        up_list.append(round(results_dict["upload"] * 9.53674E-7, 2))
         down_expected_list.append(100)
         up_expected_list.append(50)
+        ping_list.append(int(results_dict["ping"]))
 
+        Grafico(down_list, up_list, down_expected_list, up_expected_list, ping_list).plotar()
         sleep(60)
-
-    Grafico(down_list, up_list, down_expected_list, up_expected_list).plotar()
