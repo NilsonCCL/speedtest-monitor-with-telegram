84a6ef4d90fcb75d62dc90a7907a63452cd91c98
Nilson Santos <contato@nilson.com.br>
2020-07-07 22:06:03 -0300

    ajuste de json

--- main.py
+++ main.py
@@ -45,40 +45,7 @@ down_lista = []
 up_lista = []
 
 null = None
-j = subprocess.check_output(['speedtest-cli', '--json'])
-j = json.loads(j)
-
-j1 = {"client": {"rating": "0",
-                   "loggedin": "0",
-                   "isprating": "3.7",
-                   "ispdlavg": "0",
-                   "ip": "170.239.109.156",
-                   "isp": "Rede Unicon",
-                   "lon": "-35.0145",
-                   "ispulavg": "0",
-                   "country": "BR",
-                   "lat": "-8.1136"
-                   },
-        "bytes_sent": 71286784,
-        "download": 88967165.01931961,
-        "timestamp": "2020-07-07T09:27:07.293366Z",
-        "share": null,
-        "bytes_received": 112039076,
-        "ping": 24.827,
-        "upload": 56004818.92664351,
-        "server": {"latency": 24.827,
-                   "name": "Jaboat\u00e3o dos Guararapes",
-                   "url": "http://speedtest.powertecprovider.com.br:8080/speedtest/upload.php",
-                   "country": "Brazil",
-                   "lon": "-35.0089",
-                   "cc": "BR",
-                   "host": "speedtest.powertecprovider.com.br:8080",
-                   "sponsor": "Powertec",
-                   "lat": "-8.1076",
-                   "id": "27004",
-                   "d": 0.9083732997346309
-                   }
-        }
+j = json.loads(subprocess.check_output(['speedtest-cli', '--json']))
 
 down_lista.append(round(j["download"] * 9.53674E-7, 2))
 up_lista.append(round(j["upload"] * 9.53674E-7, 2))
