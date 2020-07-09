1616f2742215757e586ba7d6919af4b4034d128d
Nilson Santos <contato@nilson.com.br>
2020-07-09 13:41:22 -0300

    ordenação de variáveis inclusão do envio para o twitter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import io
 import json
+import tweepy
 import telepot
 import speedtest
 import numpy as np
@@ -9,16 +10,20 @@ import matplotlib.pyplot as plt
 with open('secrets.json', 'r') as f:
     secrets = json.load(f)
 
+auth = tweepy.OAuthHandler(secrets['consumer_key'], secrets['consumer_secret'])
+auth.set_access_token(secrets['access_token'], secrets['access_token_secret'])
+twitter = tweepy.API(auth)
+
 BOT = telepot.Bot(secrets['BOT'])
 CHAT = secrets['CHAT']
 
 
-def graph(download, upload, download_expected, upload_expected, ping):
+def graph(download_expected, upload_expected, download, upload, ping):
     plt.figure()
-    plt.plot(download, '-o', color='blue', label='Download Speed Atual')
-    plt.plot(upload, '-o', color='brown', label='Upload Speed Atual')
     plt.plot(download_expected, color='green', label='Download Speed Esperado')
     plt.plot(upload_expected, color='orange', label='Upload Speed Esperado')
+    plt.plot(download, '-o', color='blue', label='Download Speed Atual')
+    plt.plot(upload, '-o', color='brown', label='Upload Speed Atual')
     plt.plot(ping, color='purple', label='Ping')
     plt.title('Velocidade de Conexão Atual VS. Velocidade de Conexão Esperada \no teste roda a cada 5 minutos')
     plt.ylabel('Mbps')
@@ -28,9 +33,11 @@ def graph(download, upload, download_expected, upload_expected, ping):
     plt.grid(True)
     plt.xticks(color='w')
     buf = io.BytesIO()
+    plt.savefig('graph.png', format='png')
     plt.savefig(buf, format='png')
     buf.seek(0)
     send_image_to_telegram(buf)
+    send_imagem_to_twitter()
     buf.close()
     plt.close('all')
 
@@ -43,25 +50,30 @@ def make_speedtest():
     return s.results.dict()
 
 
+def send_imagem_to_twitter():
+    caption = 'github.com/NilsonCCL/speedtest-graph \n#Python #RaspberryPi #SpeedTest #Matplotlib'
+    twitter.update_with_media('graph.png', caption)
+
+
 def send_image_to_telegram(buf):
     BOT.sendPhoto(CHAT, buf)
 
 
 while True:
-    down_list = []
-    up_list = []
     down_expected_list = []
     up_expected_list = []
+    down_list = []
+    up_list = []
     ping_list = []
 
-    for _ in range(48):
+    for _ in range(1):
         results_dict = make_speedtest()
-        down_list.append(round(results_dict["download"] * 9.53674E-7, 2))
-        up_list.append(round(results_dict["upload"] * 9.53674E-7, 2))
         down_expected_list.append(100)
         up_expected_list.append(50)
+        down_list.append(round(results_dict["download"] * 9.53674E-7, 2))
+        up_list.append(round(results_dict["upload"] * 9.53674E-7, 2))
         ping_list.append(int(results_dict["ping"]))
 
-        sleep(300)
+        sleep(1)
 
     graph(down_list, up_list, down_expected_list, up_expected_list, ping_list)
